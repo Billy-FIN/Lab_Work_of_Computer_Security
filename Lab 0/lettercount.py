ec435710066f11b77dd62cb7686b6069ed258612
Billy-FIN <billyfinnn@gmail.com>
2024-01-13 18:05:27 -0500

    modified to meet homework requirements

--- Lab 0/lettercount.py
+++ Lab 0/lettercount.py
@@ -1,31 +1,24 @@
-import os
+#!/usr/bin/env python3
+import sys
 
-def lettercount(filename):
-    # Check if file exists
-    if not os.path.exists(filename):
-        return "File not found"
-    with open(filename, 'r') as file:
-        data = file.read()
-    # Remove all non-alphabetic characters
-    data = ''.join([i for i in data if i.isalpha()])
-    # all letters to lowercase
-    data = data.lower()
-    letters = {}
-    for letter in data:
-        if letter not in letters:
-            letters[letter] = 1
-        else:
-            letters[letter] += 1
-    # Sort the dictionary by key
-    order = sorted(letters)
-    output(order, letters, filename)
 
-def output(order, letters, filename):
-    with open(filename[:-4] + '-out.txt', 'w') as file:
-        for letter in order:
-            value = letters[letter]
-            file.write(f'{letter} {value}\n')
+# Read the input from stdin
+data = ''
+for line in sys.stdin:
+    data = data + line
+    if line == '':
+        break
+data = ''.join([i for i in data if i.isalpha()])
+data = data.lower()
 
-if __name__ == '__main__':
-    lettercount('test2.txt')
-
+letters = {}
+for letter in data:
+    if letter not in letters:
+        letters[letter] = 1
+    else:
+        letters[letter] += 1
+# Sort the dictionary by key
+order = sorted(letters)
+for letter in order:
+    value = letters[letter]
+    print(f'{letter} {value}')
